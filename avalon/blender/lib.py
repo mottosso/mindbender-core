115b75ac4f25eb5d007302372a70e05e2740b813
Roy Nieterau <roy_nieterau@hotmail.com>
2019-01-26 14:26:51 +0100

    Fix Blender crash on bpy.context.selected_objects because of invalid context

--- avalon/blender/lib.py
+++ avalon/blender/lib.py
@@ -2,6 +2,27 @@ import contextlib
 import bpy
 
 
+def get_selected():
+    """Return the currently selected objects in the current scene.
+
+    Note: It may seem trivial in Blender to use bpy.context.selected_objects
+        however the context may vary on how the code is being run and the
+        `selected_objects` might not be available. So this function queries
+        it explicitly from the current scene.
+
+    See: https://blender.stackexchange.com/questions/36281/bpy-context-
+         selected-objects-context-object-has-no-attribute-selected-objects
+
+    Returns:
+        list: The selected objects.
+
+    """
+    # Note that in Blender 2.8+ the code to see if an object is selected is
+    # object.select_get() as opposed to the object.select getter property
+    # it was before.
+    return [o for o in bpy.context.scene.objects if o.select_get()]
+
+
 def imprint(node, data):
     """Write `data` to `node` as user-defined data.
 
@@ -37,17 +58,24 @@ def maintained_selection():
 
     """
 
-    previous_selection = bpy.context.selected_objects
+    previous_selection = get_selected()
     previous_active = bpy.context.view_layer.objects.active
+
     try:
         yield
     finally:
         # Clear the selection
-        for node in bpy.context.selected_objects:
+        for node in get_selected():
             node.select_set(state=False)
 
         if previous_selection:
             for node in previous_selection:
-                node.select_set(state=True)
+                try:
+                    node.select_set(state=True)
+                except Exception as exc:
+                    # This could happen if a selected node was
+                    # deleted during the context
+                    print("Failed to reselect.. %s" % (exc,))
+                    continue
 
         bpy.context.view_layer.objects.active = previous_active
